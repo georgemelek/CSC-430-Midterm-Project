cc0d31303833e6cfd31c996894d38e14e35dd706
Vin Ndokaj <vinndokaj@gmail.com>
2022-04-05 17:08:41 -0400

    Functional authentication, not thoroughly tested
    rebase feature with main

--- service.py
+++ service.py
@@ -1,4 +1,6 @@
 from models import StudentModel
+from secret import SECRET_KEY
+import jwt
 
 class StudentService:
     def __init__(self):
@@ -7,3 +9,11 @@ class StudentService:
     def create(self, params):
         return self.model.create(params)
     
+    def login(self, params):
+        isValid = self.model.login(params)
+        response = {"status": "complete" if isValid else "incomplete", "jwt" : ""}
+        if isValid:
+            student_info = self.model._get_info({'username' : params['username']})
+            response['jwt'] = jwt.encode(student_info, SECRET_KEY, algorithm="HS256")
+        
+        return response
