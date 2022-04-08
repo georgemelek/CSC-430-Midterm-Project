0ca6430919505d9e6ef96f89a0b598d9abe64bf0
georgemelek <melekgeorge@outlook.com>
2022-04-08 17:19:20 -0400

    started connecting client and db, unable to decode jwt

--- client/js/index.js
+++ client/js/index.js
@@ -5,18 +5,20 @@ if (jwt == null) {
 
 function loadUser() {
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://www.mecallapi.com/api/auth/user");
+  const baseUrl = "http://127.0.0.1:5000";
+  const params = "username=george&password=george";
+  xhttp.open("POST", baseUrl + "/student/login");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.setRequestHeader("Authorization", "Bearer "+jwt);
-  xhttp.send();
+  xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+  xhttp.send(params);
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
       if (objects["status"] == "ok") {
         const user = objects["user"]
-        document.getElementById("fname").innerHTML = user["fname"];
-        document.getElementById("avatar").src = user["avatar"];
-        document.getElementById("username").innerHTML = user["username"];
+        document.getElementById("fname").innerHTML = user["first_name"];
+        // document.getElementById("avatar").src = user["avatar"];
+        // document.getElementById("username").innerHTML = user["username"];
       }
     }
   };
@@ -26,5 +28,5 @@ loadUser();
 
 function logout() {
   localStorage.removeItem("jwt");
-  window.location.href = '../login.html'
+  window.location.href = '../html/login.html'
 }
--- client/js/login.js
+++ client/js/login.js
@@ -1,6 +1,13 @@
+// import jwt_decode from 'jwt-decode-es';
+ 
+// var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJnZW9yZ2UiLCJmaXJzdF9uYW1lIjoiZ2VvcmdlIiwibGFzdF9uYW1lIjoibWVsZWsiLCJhZGRyZXNzIjoiIiwicGhvbmVfbnVtYmVyIjoiIiwiZW1haWwiOiIifQ.c5TkXb_zDMM3wsmU9l44ZHwz3zFPdb9XroRaZX5Cr5E";
+// var decoded = jwt_decode(token);
+ 
+// console.log(decoded);
+
 var jwt = localStorage.getItem("jwt");
 if (jwt != null) {
-  window.location.href = '../index.html'
+  window.location.href = '../html/index.html'
 }
 
 function login() {
@@ -8,7 +15,9 @@ function login() {
   const password = document.getElementById("password").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "https://www.mecallapi.com/api/login");
+  const baseUrl = "http://127.0.0.1:5000";
+  xhttp.open("POST", "http://localhost:5000/student/login");
+  console.log(xhttp.status);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify({
     "username": username,
@@ -26,7 +35,7 @@ function login() {
           confirmButtonText: 'OK'
         }).then((result) => {
           if (result.isConfirmed) {
-            window.location.href = '../index.html';
+            window.location.href = '../html/index.html';
           }
         });
       } else {
